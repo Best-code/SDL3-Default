cc0edd042280e1c8a1ade8b04fb7d868764d7812
Best-code <cpmraygun@gmail.com>
2025-06-03 21:50:34 -0400

    Initial Commit

---
+++ Optimize Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Optimize Project
+//
+//  Created by Melons on 6/3/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
